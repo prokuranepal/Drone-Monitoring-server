1002a0cacc58a034ce809207365d4b6f25482fea
Grunze <punksusil.khadka@gmail.com>
2018-03-20 17:47:17 +0545

    test 1

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,27 @@
-var socket = io();
-
-
-
-
+var socket = io('http://localhost:3000/');
 
 // to check connection status with the server
 socket.on('connect', function () {
+    console.log('connected successfully with the server');
+});
 
-    var params = jQuery.deparam(window.location.search);
+socket.on('copter-data', function (data) {
 
+    console.log(JSON.stringify(data,undefined,2));
     var template = jQuery('#params-data').html();
 
     var html = Mustache.render(template, {
-        params: params
+        params: JSON.stringify(data,undefined,2)
     });
+
     jQuery('#datas').append(html);
 
-    console.log('connected successfully with the server');
+    console.log(data.message);
 });
 
 // to check disconnect status
 socket.on('disconnect', function () {
     console.log('disconneted from the server');
 });
+
 
--- server/server.js
+++ server/server.js
@@ -6,8 +6,20 @@ const path = require('path');
 // it is available in core library.
 const http = require('http');
 
+// to use mysql
+/*const mysql = require('mysql');*/
+
 const express = require('express');
 const socketIO = require('socket.io');
+// mysql database
+/*
+const con = mysql.createConnection({
+    host: 'localhost',
+    database: 'swain_cordinates',
+    user: 'swain',
+    password: ''
+});
+*/
 
 const publicPath = path.join(__dirname,'..','/public');
 
@@ -23,21 +35,65 @@ var server = http.createServer(app);
 // configuring server to use socket io
 var io = socketIO(server);
 
-// setting up middleware at public directory which is displayed in browser in the main directory '/' file should be index.html
-app.use(express.static(publicPath));
 
-
+// mysql database
+/*
 app.get('/data',(req,res) => {
-    //res.setHeader('Content-Type','application/json').send(JSON.stringify(req.query,undefined,2));
+    //data = req.query;
+    con.connect( (err) => {
+        if (err) throw err;
+        var sql = "INSERT INTO location (Firmware) VALUES ('Company Inc')";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
+
     res.send("hello");
     console.log(req.query);
 });
+*/
+
+
+
+
+// setting up middleware at public directory which is displayed in browser in the main directory '/' file should be index.html
+app.use(express.static(publicPath));
+
+/*var data ;
+
+app.get('/data', (req,res) => {
+    var socket_id = [];
+    data = req.query
+   /!* io.on('connection', (socket) => {
+        console.log('connected with the client');
+        socket_id.push(socket.id);
+        if (socket_id[0] === socket.id){
+            io.removeAllListeners('connection');
+        }
+
+        socket.emit("copter-data" ,{
+            data: data
+        });
+
+        socket.on('disconnect', () => {
+            console.log('disconnected form the client');
+        });
+    });*!/
+    res.send('hello');
+});*/
+
+
 
 // to confirm the connection status with the client
 io.on('connection', (socket) => {
     console.log('connected with the client');
 
+    app.get('/data' ,(req,res) => {
 
+        io.emit('copter-data', req.query);
+        res.send('data');
+    });
 
 
     // to confirm the disconnected status with the client
@@ -45,7 +101,6 @@ io.on('connection', (socket) => {
         console.log('disconnected form the client');
     });
 });
-
 
 // setting up a server at port 3000 or describe by process.env.PORT
 server.listen(port, () => {
