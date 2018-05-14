e1e9381bea8e25d8887b50f1e87d88210b6301a7
shovan777 <raj.shrestha778@gmail.com>
2018-05-14 21:14:29 +0545

    Add db backup and collection drop feature

--- server/server.js
+++ server/server.js
@@ -14,13 +14,17 @@ const http = require('http');
 
 const express = require('express');
 const socketIO = require('socket.io');
-
+const backup = require('mongodb-backup');
 // local imports
-var {mongoose} = require('./db/mongoose');
-var {DroneData} = require('./models/droneData');
+var {
+  mongoose
+} = require('./db/mongoose');
+var {
+  DroneData
+} = require('./models/droneData');
 
-const publicPath = path.join(__dirname,'..','/public');
-const missionfile = path.join(__dirname,'..','/public/js/files/mission.txt');
+const publicPath = path.join(__dirname, '..', '/public');
+const missionfile = path.join(__dirname, '..', '/public/js/files/mission.txt');
 
 // setting the port at which the server run
 const port = process.env.PORT || 3000;
@@ -43,106 +47,137 @@ app.use(express.static(publicPath));
 app.use(bodyparser.json());
 
 let Android = [],
-    Website = [],
-    Pi = [],
-    parameters,
-    missionDownloadFlag=1;
+  Website = [],
+  Pi = [],
+  parameters,
+  missionDownloadFlag = 1;
 
 
 // to confirm the connection status with the client
 io.on('connection', (socket) => {
 
-    // Pi
-    socket.on('joinPi', () => {
-        Pi.push(socket.id);
-        socket.join('pi');
-        console.log(`${socket.id} (Pi) connected`);
-    });
+// Pi
+socket.on('joinPi', () => {
+  Pi.push(socket.id);
+  socket.join('pi');
+  console.log(`${socket.id} (Pi) connected`);
+});
 
-    socket.on('success', (msg) => {
-        // to android for successful take off
-        io.to('android').emit('success',msg);
-    });
+socket.on('success', (msg) => {
+  // to android for successful take off
+  io.to('android').emit('success', msg);
+});
 
-    socket.on('data', (data) => {
-        parameters = data;
-        io.to('website').emit('copter-data', data);
-        var droneData = new DroneData(parameters);
-        droneData.save().then(() => {
-          console.log('data has been saved.');
-        }, (e) => {
-          console.log('data cannot be saved.');
+socket.on('data', (data) => {
+  parameters = data;
+  io.to('website').emit('copter-data', data);
+  var droneData = new DroneData(parameters);
+  droneData.save().then(() => {
+    console.log('data has been saved.');
+  }, (e) => {
+    console.log('data cannot be saved.');
+  });
+});
+
+socket.on('error', (msg) => {
+  console.log(msg);
+  //missionDownloadFlag = 1;
+  // to android for error
+  io.to('website').emit('error', msg);
+});
+
+socket.on('waypoints', (waypoints) => {
+  fs.writeFile(missionfile, JSON.stringify(waypoints, undefined, 2), (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+});
+
+// Website
+socket.on('joinWebsite', () => {
+  Website.push(socket.id);
+  socket.join('website');
+  console.log(`${socket.id} (Website) connected`);
+});
+
+socket.on('getMission', (msg) => {
+  /*if (missionDownloadFlag == '1') {
+      console.log('enter');*/
+  io.to('pi').emit('mission_download', msg);
+  /*missionDownloadFlag = '0';
+      }*/
+
+});
+
+// Android
+socket.on('joinAndroid', () => {
+  Android.push(socket.id);
+  socket.join('android');
+  console.log(`${socket.id} (Android device) connected`);
+});
+
+socket.on('fly', (msg) => {
+  // send data to pi to initiate flight.
+  io.to('pi').emit('initiate_flight', msg);
+});
+
+// to confirm the disconnected status with the client
+socket.on('disconnect', () => {
+    let indexWebsite = Website.indexOf(socket.id),
+      indexAndroid = Android.indexOf(socket.id),
+      indexPi = Pi.indexOf(socket.id);
+
+    if (indexWebsite > -1) {
+      Website.splice(indexWebsite, 1);
+      console.log(`${socket.id} (Website) disconnected`);
+    }
+    if (indexAndroid > -1) {
+      Android.splice(indexAndroid, 1);
+      console.log(`${socket.id} (Android device) disconnected`);
+    }
+    if (indexPi > -1) {
+      Pi.splice(indexPi, 1);
+      parameters.conn = 'False';
+      io.to('website').emit('copter-data', parameters);
+      console.log(`${socket.id} (Pi) disconnected`);
+      // backup({
+      //   uri: process.env.MONGODB_URI,
+      //   root: './',
+      //   collection: ['dronedats'],
+      //   parser: 'json'
+      // });
+      // the above method saves all fields and also saves each document to separate json file
+      DroneData.find({}, {
+        tokens: 0,
+        __id: 0,
+        _id: 0,
+        __v: 0
+      }).
+      cursor().
+      on('data', function(doc) {
+        console.log(doc);
+        fs.appendFileSync('data.txt', doc + '\n', (err) => {
+          if (err) {
+            console.log(err);
+          }
+          // console.log('********* written to file');
         });
-    });
 
-    socket.on('error', (msg) => {
-        console.log(msg);
-        //missionDownloadFlag = 1;
-        // to android for error
-        io.to('website').emit('error',msg);
-    });
-
-    socket.on('waypoints', (waypoints) => {
-        fs.writeFile(missionfile,JSON.stringify(waypoints,undefined,2), (err) => {
-            if(err) {
-                return console.log(err);
-            }
-        });
-    });
-
-    // Website
-    socket.on('joinWebsite', () => {
-        Website.push(socket.id);
-        socket.join('website');
-        console.log ( `${socket.id} (Website) connected`);
-    });
-
-    socket.on('getMission', (msg) => {
-        /*if (missionDownloadFlag == '1') {
-            console.log('enter');*/
-            io.to('pi').emit('mission_download',msg);
-            /*missionDownloadFlag = '0';
-        }*/
+      }).
+      on('end', function() {
+        // userStream.close();
+        DroneData.collection.drop();
+        console.log('********** the file has been written and db is dropped.');
+      });
 
     });
+}
 
-    // Android
-    socket.on('joinAndroid',() => {
-        Android.push(socket.id);
-        socket.join('android');
-        console.log(`${socket.id} (Android device) connected`);
-    });
-
-    socket.on('fly', (msg) => {
-        // send data to pi to initiate flight.
-        io.to('pi').emit('initiate_flight',msg);
-    });
-
-    // to confirm the disconnected status with the client
-    socket.on('disconnect', () => {
-        let indexWebsite = Website.indexOf(socket.id),
-            indexAndroid = Android.indexOf(socket.id),
-            indexPi = Pi.indexOf(socket.id);
-
-        if (indexWebsite > -1) {
-            Website.splice(indexWebsite,1);
-            console.log(`${socket.id} (Website) disconnected`);
-        }
-        if (indexAndroid > -1) {
-            Android.splice(indexAndroid,1);
-            console.log(`${socket.id} (Android device) disconnected`);
-        }
-        if (indexPi > -1 ){
-            Pi.splice(indexPi,1);
-            parameters.conn = 'False';
-            io.to('website').emit('copter-data', parameters);
-            console.log(`${socket.id} (Pi) disconnected`);            
-        }
-
-    });
+});
 });
 
 // setting up a server at port 3000 or describe by process.env.PORT
 server.listen(port, () => {
-    console.log(`Server running at ${port}`);
+  console.log(`Server running at ${port}`);
 });
