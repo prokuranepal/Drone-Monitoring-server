b760d0b819912849fb66a8844cbcfc38bf5d4c83
Grunze1s <070bex446@ioe.edu.np>
2018-12-26 00:45:04 +0545

    removed token based and added session based passport authentication

--- server/auth/authenticate.js
+++ server/auth/authenticate.js
@@ -1,19 +1,16 @@
-const {User} = require('../models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-let authenticate = (req, res, next) => {
-    console.log(req.headers);
-    let token = req.header('x-auth');
-    console.log(`${token} inside authenticate`);
-    User.findByToken(token).then((user) => {
-        if (!user) {
-            return Promise.reject();
-        }
-        req.user = user;
-        req.token = token;
-        next();
-    }).catch((err) => {
-        res.status(401).send();
+const User = require('../models/user');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser((user,done) => {
+    done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id, (err,user) => {
+        done(err,user);
     });
-};
-
-module.exports = {authenticate};
+});
